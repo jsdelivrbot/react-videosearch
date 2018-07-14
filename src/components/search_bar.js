e25877d6919c44e83be473a07bb7b1baafbc81c6
Rashmi Kamath <rashmikkamath@gmail.com>
2018-07-14 19:29:37 -0400

    Video SearchBar component.

---
+++ src/components/search_bar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+//This component has an input for searching a video.
+
+const SearchBar = () => {
+	return <input />
+};
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+//youtube api key
+const API_KEY = 'AIzaSyBe5s8YxNgzgue4g-mwxE9WB53vwgsGyA0';
 
 //Create a new component.
-
-const App = () => <div>Hi</div>;
+const App = () => {
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
+}
 
 //Take this component's generated html and put into page(DOM)
-
 ReactDOM.render(<App />, document.querySelector('.container'));
