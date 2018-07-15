09f73135dcd443f614bbe64d1d9b67df56163029
Rashmi Kamath <rashmikkamath@gmail.com>
2018-07-15 09:56:07 -0400

    Functional component that contains the list of videos.

--- src/index.js
+++ src/index.js
@@ -2,19 +2,30 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 //youtube api key
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-	console.log(data);
-});
-
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			videos : []
+		};
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+			//this.setState({videos:videos}) when prop and key are same variable name
+		});
+	}
 	render() {
+		
 		return (
 			<div>
 				<SearchBar />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
