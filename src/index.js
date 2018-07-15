259bc7a79f63c4c50d379d710b9cdbbd04463eff
Rashmi Kamath <rashmikkamath@gmail.com>
2018-07-15 13:11:16 -0400

    Styling fixes. Search feature implemented.

--- src/index.js
+++ src/index.js
@@ -3,29 +3,43 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 //youtube api key
 const API_KEY = process.env.REACT_APP_API_KEY;
+
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			videos : []
+			videos : [],
+			selectedVideo : null
 		};
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos });
-			//this.setState({videos:videos}) when prop and key are same variable name
+		this.videoSearch('skunk');
+
+	}
+
+	videoSearch (term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({ 
+				videos :videos,
+				selectedVideo : videos[0]
+			 });
+			
 		});
+
 	}
 	render() {
 		
 		return (
 			<div>
-				<SearchBar />
-				<VideoList videos={this.state.videos} />
+				<SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList videos={this.state.videos} onVideoSelect={selectedVideo => this.setState({selectedVideo})} />
+
 			</div>
 		);
 	}
