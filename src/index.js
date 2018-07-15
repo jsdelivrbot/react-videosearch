1dd26bb5fdf1914047a91135cbdbbee782fc8b33
Rashmi Kamath <rashmikkamath@gmail.com>
2018-07-15 00:57:56 -0400

    Youtube api search. App is a class based component.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 //youtube api key
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
-//Create a new component.
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+	console.log(data);
+});
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
-
-//Take this component's generated html and put into page(DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
