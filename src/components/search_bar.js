b98e234c099c1b86221a68ba5f4ea78adc3dfa95
Rashmi Kamath <rashmikkamath@gmail.com>
2018-07-15 00:19:20 -0400

    Controlled input value

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,8 +3,20 @@ import React, {Component} from 'react';
 
 //class SearchBar extends React.Component
 export default class SearchBar extends Component {
-	render() {
-		return <input onChange={event => console.log(event.target.value) }/>;
-	}
+	constructor(props) {
+		super(props);
 
+		this.state = {
+			term: ''
+		};
+	}
+	render() {
+		return (
+		<div>
+			<input 
+			value = {this.state.term}
+			onChange={event => this.setState({ term: event.target.value }) }/>
+		</div>
+		);
+	}
 }
